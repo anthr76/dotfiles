10a9708ea9ec69320541ce0f46405ddc617fd3c9
Anthony Rabbito <hello@anthonyrabbito.com>
2021-09-16 14:09:50 -0400

    fix: remove fish greeting
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -3,6 +3,7 @@ set -gx KUBECONFIG $HOME/.kube/config
 set -gx VISUAL nvim
 set -gx EDITOR nvim
 set -gx GPG_TTY (tty)
+set -U fish_greeting
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
