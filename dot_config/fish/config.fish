e22bdba084100e6c062008a0dc71c3aeb74a95d4
Anthony Rabbito <hello@anthonyrabbito.com>
2021-09-22 19:32:32 -0400

    fix: molecule env
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -3,6 +3,9 @@ set -gx KUBECONFIG $HOME/.kube/config
 set -gx VISUAL nvim
 set -gx EDITOR nvim
 set -gx GPG_TTY (tty)
+set -gx ANSIBLE_FORCE_COLOR true
+set -gx PY_COLORS true
+set -gx MAK_MOLECULE_DRIVER podman
 set -U fish_greeting
 
 if status is-interactive
