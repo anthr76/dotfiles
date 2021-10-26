fa25f5705e64911675c9eb0043e788abd525b8f0
Anthony Rabbito <hello@anthonyrabbito.com>
2021-10-26 12:08:06 -0400

    fix: remove old work secrets
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- dot_oh-my-zsh/custom/functions.zsh
+++
@@ -1,7 +0,0 @@
-stern () {
-podman-host run --userns keep-id --rm -it -v "$HOME/.kube:/$HOME/.kube":z -e KUBECONFIG="$KUBECONFIG" ghcr.io/stern/stern "$@"
-}
-
-butane () {
-podman-host run --rm --tty --interactive --security-opt label=disable --volume ${PWD}:/pwd --workdir /pwd  quay.io/coreos/butane:release "$@"
-}
