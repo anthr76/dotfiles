eeb2cea67671f3c9b774b80f275215d94cf4bddc
Anthony Rabbito <hello@anthonyrabbito.com>
2021-11-02 18:59:15 -0400

    fix: update dotfiles for home yubi
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- dot_config/fish/functions/stern.fish
+++
@@ -1,3 +0,0 @@
-function stern --wraps=podman --description 'stern in docker'
-  podman run --userns keep-id --rm -it -v "$HOME/.kube:/$HOME/.kube":z -e KUBECONFIG="$KUBECONFIG" ghcr.io/stern/stern $argv
-end
