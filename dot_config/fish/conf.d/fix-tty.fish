35d9bb48e5342c8322843a354b0d7972fec14d40
Anthony Rabbito <hello@anthonyrabbito.com>
2022-01-31 18:44:33 -0500

    feat: add ssh-agent systemd service
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- dot_config/fish/conf.d/fix-tty.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-#
-# The purpose of this script relates around the findings in https://github.com/containers/toolbox/issues/568
-#
-if test -f /run/.containerenv
-    sudo chown anthonyjrabbito:tty (tty)
-end
